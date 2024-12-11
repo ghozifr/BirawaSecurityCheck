f5b05a9194bd327f38c5428ba5635cdcac4edb42
ghozi <ghozifr@gmail.com>
2024-12-11 13:25:20 +0700

    reset

--- backend/src/routes/_TestRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import TestController from '../controllers/_TestController'
-
-const router = express.Router()
-
-router.get('/', TestController.test)
-
-export default router
--- frontend/src/pages/ForgotPasswordPage.tsx
+++
@@ -1,9 +0,0 @@
-import ForgotPass from '@/components/custom/organism/ForgotPass'
-
-export default function forgotpassword() {
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-        <ForgotPass/>
-    </div>
-  )
-}
